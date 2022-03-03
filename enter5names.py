76439f7be2209306eefff1d2034f237739117864
sherlocksameer7 <arijitsameers@gmail.com>
2022-03-03 16:09:33 +0530

    enter 5 names has been added

--- enter5names.py
+++ enter5names.py
@@ -1,8 +1,11 @@
 name_list = []
+email_list = []
 for i in range(0, 5):
     name = input("Enter names ")
+    email = input("Enter email id ")
     name_list.append(name)
-name_list.sort()
+    email_list.append(email.lower())
 print(name_list)
+print(email_list)
 
 
